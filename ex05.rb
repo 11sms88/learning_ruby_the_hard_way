f7f76b264bc7130dd01d81f265632721f34cdb60
Samantha <11sms88@gmail.com>
2016-02-05 00:34:02 -0600

    Conversion
    write variables that convert inches and pounds to centimeters and kilograms. Use the math in Ruby.

--- ex05.rb
+++ ex05.rb
@@ -5,10 +5,13 @@ weight = 180 #lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
+kilos = weight / 2.2 #this by itself prints repeating remiander
 
 puts "Let's talk about #{name}."
 puts "He's #{height} inches tall."
+puts "He's #{height * 2.54} centimeters tall." # in * 2.54 = cm; cm * 0.39 = in
 puts "He's #{weight} pounds heavy."
+puts "He's #{(kilos).round(2)} kilograms heavy." # kg = lb/2.2046
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."

e6ac4d38295fdc3921a394c4e5778b02e6645c8f
Samantha <11sms88@gmail.com>
2016-02-06 00:00:40 -0600

    Escape Sequences
    Trying different escape sequences to see what they do...

--- ex10.rb
+++ ex10.rb
@@ -13,3 +13,16 @@ puts tabby_cat
 puts persian_cat
 puts backslash_cat
 puts fat_cat
+
+#Try the other escape sequences to see what they do
+
+print "What does \athis do?" #ASCII BEL
+print "This is a \bbackspace" #ASCII BS
+print "form\ffeed" #FF
+
+print """
+Red \rhair crookshanks bludger Marauder’s Map 
+\tProngs \nsunshine daisies \rbutter \vmellow Ludo Bagman. 
+Beaters gobbledegook N.E.W.T., Honeydukes eriseD 
+inferi Wormtail. Mistletoe dungeons Parseltongue
+"""

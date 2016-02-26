d71c62c240390bba88679daa377230dc3492cd6a
Samantha Smith <11sms88@gmail.com>
2016-02-26 12:55:13 -0600

    Fix typos

--- ex21.rb
+++ ex21.rb
@@ -23,7 +23,7 @@ puts "Let's do some math with just functions!"
 
 age = add(30, 5)
 height = subtract(78, 4)
-weight = multiplying(90, 2)
+weight = multiply(90, 2)
 iq = divide(100, 2)
 
 puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
@@ -32,6 +32,6 @@ puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
 # A puzzle for the extra credit, type it in anyway.
 puts "Here is a puzzle."
 
-what = add(age, subtract(height, multiply (weight, divide(iq, 2))))
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 puts "That becomes: #{what}. Can you do it by hand?"

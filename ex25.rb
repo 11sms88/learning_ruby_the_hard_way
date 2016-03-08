24ff13e5601f52c2a6b4dd097051f72dbb2d31f0
Samantha Smith <11sms88@gmail.com>
2016-03-08 17:47:44 -0600

    Even More Practice-full

--- ex25.rb
+++ ex25.rb
@@ -32,3 +32,15 @@ module Ex25
 	# Prints the first and last words of the sentence.
 	def Ex25.print_first_and_last(sentence)
 		words = Ex25.break_words(sentence)
+		Ex25.print_first_word(words)
+		Ex25.print_last_word(words)
+	end
+
+	#Sorts the words then prints the first and last one.
+	def Ex25.print_first_and_last_sorted(sentence)
+		words = Ex25.sort_sentence(sentence)
+		Ex25.print_first_word(words)
+		Ex25.print_last_word(words)
+	end
+
+end
